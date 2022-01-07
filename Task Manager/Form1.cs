15097bacf81d96f0da640168c204cff738f20124
Luciano <luciano.scp@hotmail.com>
2022-01-06 23:19:03 -0300

    Add processes to listView

--- Task Manager/Form1.cs
+++ Task Manager/Form1.cs
@@ -8,6 +8,11 @@ namespace Task_Manager
         public Form1()
         {
             InitializeComponent();
+            Process[] locallAll = Process.GetProcesses();
+            foreach (Process process in locallAll)
+            {
+                listView1.Items.Add(process.ProcessName);
+            }
         }
     }
 }
